398587c95f123daf1f56cf16a1871fb935555589
Lyndsey Reese <lmariereese@gmail.com>
2019-09-26 12:57:27 -0400

    Imported redux

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import {Provider} from 'react-redux';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Oh hey</Text>
     </View>
   );
 }
@@ -17,3 +18,16 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <Provider store={store}>
+//         <View style={styles.container}>
+//           <Text>Oh hey</Text>
+//         </View>
+//       </Provider>
+//     )
+//   }
+// }
+
