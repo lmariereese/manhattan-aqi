038b5d4400e897522557400842f89da7e01b8254
Lyndsey Reese <lmariereese@gmail.com>
2019-10-14 15:19:31 -0400

    Removed unused files

--- notes.js
+++ notes.js
@@ -1,17 +1,3 @@
-// var Circle = React.createClass({
-//   render: function() {
-//       return (
-//           <View style={styles.circle} />
-//       )
-//   }
-// })
-
-// circle: {
-//   width: 100,
-//   height: 100,
-//   borderRadius: 100/2,
-//   backgroundColor: 'red'
-// }
 
 /*
 
